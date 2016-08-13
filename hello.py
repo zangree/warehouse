5a981bfcba6b749b6b7df27915f5ce38c644a5f3
zangree <zangree@163.com>
2016-08-13 16:05:35 +0800

    add templates

--- hello.py
+++ hello.py
@@ -19,5 +19,15 @@ def index():
 def user(name):
     return render_template('user.html', name=name)
 
+
+@app.errorhandler(400)
+def page_not_found(e):
+    return render_template('404.html'), 400
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 if __name__ == '__main__':
     manager.run()
