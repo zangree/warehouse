1a7106071c4e477d7a3a415b9274d191bc01b04e
zangree <zangree@163.com>
2016-08-13 14:58:49 +0800

    bootstrap test

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
@@ -12,8 +12,12 @@ bootstrap = Bootstrap(app)
 
 @app.route("/")
 def index():
-	user_agent = request.headers.get('User-Agent')
-	return "<p>Your browser is %s</p>" % user_agent
+    return render_template("index.html")
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
 
 if __name__ == '__main__':
-	manager.run()
+    manager.run()
