4875f1b980c009ca1a516ec0323b432ed4962ac4
zangree <zangree@163.com>
2016-08-14 13:35:54 +0800

    wtf

--- hello.py
+++ hello.py
@@ -4,15 +4,28 @@ from flask import Flask, render_template
 from flask import request
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "jajgaijgakg;1894412!!!"
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 
+class NameFrom(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+     name = None
+     form = NameFrom()
+     if form.validate_on_submit():
+         name = form.name.data
+         form.name.data = ''
+     return render_template("index.html", form=form, name=name)
 
 
 @app.route('/user/<name>')
