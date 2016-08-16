bab52adb65f709985d5ee607d1739606b89d4484
zangree <zangree@163.com>
2016-08-16 15:18:01 +0800

    view database 5b

--- hello.py
+++ hello.py
@@ -33,7 +33,7 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref="role")
+    users = db.relationship('User', backref="role", lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
@@ -53,12 +53,18 @@ class User(db.Model):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ""
         return redirect(url_for('index'))
-    return render_template("index.html", form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('known', False))
 
 
 @app.route('/user/<name>')
