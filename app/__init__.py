93e361875e094a0fba6412053908c7651959fbb0
zangree <zangree@163.com>
2016-08-25 00:29:43 +0800

    delete some unuseful codes and files and fixed a mistake

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
