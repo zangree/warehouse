c37059186662b06dd9d25bfa39ce75d3105a8d5a
zangree <zangree@163.com>
2016-08-28 16:45:53 +0800

    Login

---
+++ app/auth/forms.py
@@ -0,0 +1,12 @@
+# -*-coding:utf-8 -*-
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
+
+
+class LoginForm(Form):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
+                                             Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Login In')
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
-from . import db
+from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -16,8 +17,10 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     @property
     def password(self):
@@ -32,3 +35,8 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
